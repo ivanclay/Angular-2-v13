c30f5dec8b1e18ee44be6fd7c0bd0d64621c947a
ivanclay <ivan.clay.moura@gmail.com>
2022-03-11 08:25:47 -0300

    Event Binding

--- curso-angular-2-v13/src/app/data-binding/data-binding.component.ts
+++ curso-angular-2-v13/src/app/data-binding/data-binding.component.ts
@@ -13,9 +13,29 @@ export class DataBindingComponent implements OnInit {
   public btnDisabled: boolean = true;
   public imgSrc: string = 'https://vidafullstack.com.br/wp-content/uploads/2021/12/banner.jpg';
 
+  public position: {x: number, y: number} = {x:0, y:0};
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public alertaInfo(): void {
+    alert('clicou no botão!');
+  }
+
+
+  public alertaInfoParameter(valor:string): void {
+    alert('clicou no botão com parametro!');
+  }
+
+public alertaInfoParameterEvento(event: MouseEvent): void{
+  console.log(event);
+}
+
+public mouseMoveEvent(event: MouseEvent): void{
+  this.position.x = event.offsetX;
+  this.position.y = event.offsetY;
+}
 
 }
