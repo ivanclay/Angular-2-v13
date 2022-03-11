159cabbb699b605adb072597e9d14c9474792dab
ivanclay <ivan.clay.moura@gmail.com>
2022-03-11 07:34:58 -0300

    Aplicando o root direto no app.component.

--- curso-angular-2-v13/src/app/app.component.ts
+++ curso-angular-2-v13/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  template: '<router-outlet></router-outlet>'
+}) //fazendo de um modo diferente
 export class AppComponent {
   title = 'curso-angular-2-v13';
 }
