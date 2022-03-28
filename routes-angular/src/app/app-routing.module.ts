8a4ec91eb8dc04996e99c64bde2495a44430df3e
ivanclay <ivan.clay.moura@gmail.com>
2022-03-28 08:24:26 -0300

    Estudando sobre rotas.

--- routes-angular/src/app/app-routing.module.ts
+++ routes-angular/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { SobreComponent } from './pages/sobre/sobre.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   // { path: 'home', component: HomeComponent },
-  { path: 'sobre', component: SobreComponent },
+  { path: 'sobre/:id/:username', component: SobreComponent },
   { path: '404', component: PageErrorComponent },
   { path: '**', redirectTo: '404' }
 ];
--- routes-angular/src/app/pages/sobre/sobre.component.ts
+++ routes-angular/src/app/pages/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      res => console.log(res['id'], res['username'], res)
+    );
   }
 
 }
