87fa8bad35eaa151569adc32803c8925dd86aec1
ivanclay <ivan.clay.moura@gmail.com>
2022-03-30 07:19:36 -0300

    Lesson 87: import pokeApi

--- pokedex-angular/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex-angular/src/app/shared/poke-list/poke-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/services/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
+
 export class PokeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pokeService: PokeApiService) { }
 
   ngOnInit(): void {
+    this.pokeService.apiListAllPokemons.subscribe(
+      res => res
+    );
   }
 
 }
