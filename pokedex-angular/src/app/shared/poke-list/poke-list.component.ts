f365d6a87c96e67a3752c4caab810184cc483613
ivanclay <ivan.clay.moura@gmail.com>
2022-03-30 07:50:55 -0300

    Lesson 89: Loading pokemon list.

--- pokedex-angular/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex-angular/src/app/shared/poke-list/poke-list.component.ts
@@ -9,11 +9,16 @@ import { PokeApiService } from 'src/app/services/poke-api.service';
 
 export class PokeListComponent implements OnInit {
 
+  public getAllPokemons: any;
+
   constructor(private pokeService: PokeApiService) { }
 
   ngOnInit(): void {
     this.pokeService.apiListAllPokemons.subscribe(
-      res => res
+      res => {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons);
+      }
     );
   }
 
