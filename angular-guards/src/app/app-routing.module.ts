2df07442b467978b6b42341708756ed2c0e07ab3
ivanclay <ivan.clay.moura@gmail.com>
2022-03-31 08:12:25 -0300

    Guards

---
+++ angular-guards/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//PAGES
+import { AccountComponent } from './shared/account/account.component';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
+import { CanDeactiveGuard } from './shared/guards/can-deactive.guard';
+import { HomeComponent } from './shared/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'account', component: AccountComponent, canActivate: [CanActiveGuard], canDeactivate: [CanDeactiveGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
